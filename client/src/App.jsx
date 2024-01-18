6d784cec3697c14a139718d57783697f614faf63
prashant kumar <prashant.kumar.21@aot.edu.in>
2024-01-18 21:03:56 +0530

    create-pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+import Home from './pages/Home.jsx'  
+import Signup from './pages/Signup.jsx'
+import Signin from './pages/Signin.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Project from './pages/Project.jsx'
+import About from './pages/About.jsx'
 
 function App() {
   return (
-    <div className="text-3xl bg-red-300 text-amber-500">App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/Signup' element={<Signup />}/>
+      <Route path='/Signin' element={<Signin />}/>
+      <Route path='/Dashboard' element={<Dashboard />}/>
+      <Route path='/Project' element={<Project />}/>
+      <Route path='/About' element={<About />}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
