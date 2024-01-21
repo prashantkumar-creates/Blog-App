c15b1addfb757f0841fe49c968d1c564ec470acd
prashant kumar <prashant.kumar.21@aot.edu.in>
2024-01-21 13:46:24 +0530

    Connection to data base (Mogodb)

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
+import { error } from 'console';
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
 
 const app = express();
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("mongodb is connected ")
+}).catch((error) =>{
+    console.log(error);
+})
+
 
 app.get('/',(req,res) => {
     res.send("Home is sending something")
