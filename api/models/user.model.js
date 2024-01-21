bf9ad40c1f6af33faa1c120750bd48d94afc4b58
prashant kumar <prashant.kumar.21@aot.edu.in>
2024-01-21 14:03:32 +0530

    added Password element  in userModel

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
         type:String,
         require:true,
         unique:true
+    },
+    password:{
+        type:String,
+        require:true
     }
 },{timestamps:true})
 
