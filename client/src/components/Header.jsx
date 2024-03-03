a274b5bba133e23c33f6b60f4738083ba73e1812
prashant kumar <prashant.kumar.21@aot.edu.in>
2024-03-03 12:35:38 +0530

    Add user avatar and dropdown menu to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import { Navbar, TextInput, Button } from "flowbite-react"
+import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react"
 import { Link ,useLocation} from "react-router-dom"
 import { AiOutlineSearch } from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 function Header() {
     var path = useLocation().pathname;
+    const  {currentUser} = useSelector(state => state.user)
+    console.log(currentUser);
     return (
         <Navbar className="border-b-2">
-            <Link className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white
+            <Link to='/'className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white
           '>
                 <span className='px-2 py-1 bg-gradient-to-r from-blue-500 to-orange-500
           rounded-lg text-white'>Cricket</span>
@@ -16,39 +19,63 @@ function Header() {
             <form action="">
                 <TextInput
                     type="text"
-                    placeholder="Search"
+                    placeholder="Search..."
                     rightIcon={AiOutlineSearch}
                     className="hidden lg:inline"
                 />
             </form>
             
             <Button className="w-12 h-10 lg:hidden" color="gray" pill> <AiOutlineSearch /></Button>
-            <div className="flex gap-3">
+             
+            <div className='flex gap-2 md:order-2'>
                 <Button className="w-12 h-10 sm:inline" color="gray" pill>
                     <FaMoon />
                 </Button>
-                <Link to='/Signin' >
-                    <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
-                </Link>
+                {currentUser ? (
+                    <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                      <Avatar alt='user' img={currentUser.profilePic} rounded />
+                      
+                    }
+                  >
+                    <Dropdown.Header>
+                      <span className='block text-sm'>@{currentUser.username}</span>
+                      <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                    </Dropdown.Header>
+                    <Link to={'/dashboard?tab=profile'}>
+                      <Dropdown.Item>Profile</Dropdown.Item>
+                    </Link>
+                    <Dropdown.Divider />
+                    <Dropdown.Item>Sign out</Dropdown.Item>
+                  </Dropdown>
+                ): (
+                    <Link to='/signin'>
+                      <Button gradientDuoTone='purpleToBlue' outline>
+                        Sign In
+                      </Button>
+                    </Link>
+                  )}
                 <Navbar.Toggle/>
             </div>
             <Navbar.Collapse>
-                    <Navbar.Link active={path === "/"} as='div'>
+                    <Navbar.Link active={path === '/'} as={'div'}>
                         <Link to='/'>
                           Home
                         </Link>
                     </Navbar.Link>
-                    <Navbar.Link active={path === "/about"} as='div'>
+                    <Navbar.Link active={path === '/about'} as={'div'}>
                         <Link to='/about'>
                           About
                         </Link>
                     </Navbar.Link>
-                    <Navbar.Link active={path === "/project"} as='div'>
+                    <Navbar.Link active={path === '/project'} as={'div'}>
                         <Link to='/project'>
                           Project
                         </Link>
                     </Navbar.Link>
-             </Navbar.Collapse>   
+             </Navbar.Collapse> 
            
         </Navbar>
     )
